87285432ae849582c7fb555324b1b6df254dc9b4
motyliak <67186212+motyliak@users.noreply.github.com>
2020-06-23 21:13:32 +0200

    pridane aj ovladanie pomocou A a B buttonov, na P2 pripojeny neopixel

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
+def motor_off():
+    pins.digital_write_pin(DigitalPin.P0, 0)
+    basic.show_icon(IconNames.NO)
+
+def on_button_pressed_a():
+    global remote_control
+    remote_control = False
+    motor_on()
+    strip.rotate(1)
+    strip.show()
+    
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global remote_control
+    motor_off()
+    remote_control = True
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def motor_on():
+    pins.digital_write_pin(DigitalPin.P0, 1)
+    basic.show_icon(IconNames.YES)
+remote_control = False
 pins.digital_write_pin(DigitalPin.P0, 1)
 pins.set_pull(DigitalPin.P1, PinPullMode.PULL_UP)
 basic.show_icon(IconNames.HAPPY)
+remote_control = True
+strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
+strip.show_rainbow(1, 360)
+strip.show()
 
 def on_forever():
-    if pins.digital_read_pin(DigitalPin.P1) == 0:
-        pins.digital_write_pin(DigitalPin.P0, 1)
-        basic.show_icon(IconNames.YES)
-    else:
-        pins.digital_write_pin(DigitalPin.P0, 0)
-        basic.show_icon(IconNames.NO)
+    strip.show()
+    if remote_control:
+        if pins.digital_read_pin(DigitalPin.P1) == 0:
+            motor_on()
+        else:
+            motor_off()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,13 +1,42 @@
+function motor_off() {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.showIcon(IconNames.No)
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    remote_control = false
+    motor_on()
+    strip.rotate(1)
+    strip.show()
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    motor_off()
+    remote_control = true
+})
+function motor_on() {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.showIcon(IconNames.Yes)
+}
+
+let remote_control = false
 pins.digitalWritePin(DigitalPin.P0, 1)
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 basic.showIcon(IconNames.Happy)
+remote_control = true
+let strip = neopixel.create(DigitalPin.P2, 8, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+strip.show()
 basic.forever(function on_forever() {
-    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.showIcon(IconNames.Yes)
-    } else {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        basic.showIcon(IconNames.No)
+    strip.show()
+    if (remote_control) {
+        if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+            motor_on()
+        } else {
+            motor_off()
+        }
+        
     }
     
 })
